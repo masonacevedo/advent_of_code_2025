61dbb5b592878211c206685b3d2bc15458d6a052
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-06-03 22:22:55 -0700

    day 3 part 2 done

--- 3_problem.py
+++ 3_problem.py
@@ -1,25 +1,38 @@
-f_name = "3_example.txt"
+f_name = "3_input.txt"
 
 with open(f_name, "r+") as f:
     lines = f.readlines()
 
-def calcBest(L):
-    if len(L) == 12:
-        return L
+def calcBest(L, budget):
+    if budget == len(L):
+        return int(L)
+
+    if budget > len(L):
+        return -1
+
+    if budget == 1:
+        return max([int(char) for char in L])
+
+    for i in range(9, 0, -1):
+        i_str = str(i)
+        if i_str not in L:
+            continue
+        startIndex = L.index(i_str)
+        # print("L:             ", L)
+        # print("L[startIndex:]:", L[startIndex+1:])
+        # input("enter to con")
+        subSol = calcBest(L[startIndex+1:], budget-1)
+        if subSol != -1:
+            return int(str(L[startIndex]) + str(subSol))
+
     
-    # print("L:", L)
-    possibilities = []
-    for i in range(0, len(L)):
-        s = L[0:i] + L[i+1:]
-        possibilities.append(int(calcBest(s)))
-    
-    return str(max(possibilities))
+    return ans
 
 
 def getHighest(line):
     clean_line = line.replace("\n", "")
     # print("clean_line:", clean_line)
-    ans = calcBest(clean_line)
+    ans = calcBest(clean_line, 12)
     # print("ans:", ans)
     # print()
     return ans
