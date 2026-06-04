b710247434eaf4af9467024f4ef61290c62d232c
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-06-03 17:26:09 -0700

    day 2 part 2

--- 2_problem.py
+++ 2_problem.py
@@ -6,15 +6,28 @@ with open(f_name, "r+") as f:
 ranges = lines[0].split(",")
 
 
+def chopWorks(s, i):
+    # return true if s is just the first i characters
+    # repeated some number of times, false otherwise.
+    # print("i:", i)
+    # print("s:", s)
+    prefix = s[0:i]
+    # print("prefix:", prefix)
+    # print("prefix*i:", prefix * (len(s)//i))
+    answer = (s == prefix * (len(s)//i))
+    # print('answer:', answer)
+    # input("enter to con")
+    return answer
 
 def invalid(num):
     s = str(num)
-    if len(s) % 2 == 1:
-        return False
-    
-    leftHalf = s[0:len(s)//2]
-    rightHalf = s[len(s)//2:]
-    return leftHalf == rightHalf
+    for i in range(1, (len(s)//2)+1):
+        if chopWorks(s, i):
+            # print("s:", s)
+            # print("i:", i)
+            # input('enter to con')
+            return True
+    return False
 
 invalid_IDs = []
 for r in ranges:
