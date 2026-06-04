2a003ad4de570459bbe1171760151ca1acbd6d49
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-06-03 22:08:40 -0700

    day 3 part 2 in progress

--- 3_problem.py
+++ 3_problem.py
@@ -1,22 +1,30 @@
-f_name = "3_input.txt"
+f_name = "3_example.txt"
 
 with open(f_name, "r+") as f:
     lines = f.readlines()
 
-def getHighest(line):
-    # print("line:", line)
-    clean_line = line.replace("\n", "")
-    bestSoFar = float('-inf')
-    for i in range(0, len(clean_line)):
-        for j in range(i+1, len(clean_line)):
-            leftNum = line[i]
-            rightNum = line[j]
-            num = 10*int(leftNum) + int(rightNum)
-            bestSoFar = max(num, bestSoFar)
+def calcBest(L):
+    if len(L) == 12:
+        return L
     
-    # print("bestSoFar:", bestSoFar)
-    # print()
-    return bestSoFar
+    # print("L:", L)
+    possibilities = []
+    for i in range(0, len(L)):
+        s = L[0:i] + L[i+1:]
+        possibilities.append(int(calcBest(s)))
+    
+    return str(max(possibilities))
 
-values = [getHighest(line) for line in lines]
+
+def getHighest(line):
+    clean_line = line.replace("\n", "")
+    # print("clean_line:", clean_line)
+    ans = calcBest(clean_line)
+    # print("ans:", ans)
+    # print()
+    return ans
+
+
+
+values = [int(getHighest(line)) for line in lines]
 print(sum(values))
