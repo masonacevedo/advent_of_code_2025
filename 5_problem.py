f371e469fbee706f6d42a6d778172ef0af407a2f
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-06-04 08:44:59 -0700

    day 5 part 2

--- 5_problem.py
+++ 5_problem.py
@@ -9,19 +9,38 @@ clean_lines = [line.replace("\n", "") for line in lines]
 midpoint = clean_lines.index("")
 
 print()
-print()
-print()
+
+
+
+def mergeSortedIntervals(leftInterval, rightInterval):
+    # print("leftInterval:", leftInterval)
+    if leftInterval[1] < rightInterval[0]:
+        return [leftInterval, rightInterval]
+    return [[leftInterval[0], max(leftInterval[1], rightInterval[1])]]
+
 
 freshRanges = clean_lines[0:midpoint]
+intervals = []
+for rangeString in freshRanges:
+    left, right = rangeString.split("-")
+    intervals.append([int(left), int(right)])
 
-ings = clean_lines[midpoint+1:]
 
-def isFresh(ing):
-    for r in freshRanges:
-        lower, upper = r.split("-")
-        if int(lower) <= ing and ing <= int(upper):
-            return True
-    return False
+sortedIntervals = sorted(intervals, key = lambda i: i[0])
+# print(sortedIntervals)
+i = 0
+while i < len(sortedIntervals)-1:
+    merge = mergeSortedIntervals(sortedIntervals[i], sortedIntervals[i+1])
+    if len(merge) == 2:
+        i += 1
+        continue
+    sortedIntervals[i] = merge[0]
+    del sortedIntervals[i+1]
 
-freshIngs = [isFresh(int(ing)) for ing in ings]
-print(sum(freshIngs))
+# for i in sortedIntervals:
+#     print(i)
+
+def freshCount(interval):
+    return interval[1] - interval[0] + 1
+
+print(sum([freshCount(i) for i in sortedIntervals]))
