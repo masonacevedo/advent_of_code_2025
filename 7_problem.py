3651760bb4e0392228c50c1c816450d04582e29e
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-06-04 12:57:40 -0700

    7 almost working

--- 7_problem.py
+++ 7_problem.py
@@ -1,6 +1,6 @@
 import math
 
-f_name = "7_input.txt"
+f_name = "7_example.txt"
 
 with open(f_name, "r+") as f:
     lines = f.readlines()
@@ -10,7 +10,7 @@ grid = [[char for char in line] for line in grid]
 
 def printGrid(grid):
     for row in grid:
-        print("".join(row))
+        print("".join([str(e) for e in row]))
 
 printGrid(grid)
 
@@ -32,5 +32,60 @@ def propogate(G, splitCount):
     return splitCount
 
 splitCount = propogate(grid, 0)
+print("\n")
 printGrid(grid)
-print("splitCount:", splitCount)
+
+nums = [1,2,3,4,5,6,7,8,9]
+
+firstRow = 0
+firstCol = grid[0].index("|")
+grid[firstRow][firstCol] = 1
+print("\n")
+printGrid(grid)
+
+def count():
+    for row in range(0, len(grid)):
+        for col in range(0, len(grid[0])):
+            if grid[row][col] not in nums:
+                continue
+            if row == len(grid)-1:
+                continue
+            currentNum = grid[row][col]
+            if grid[row+1][col] == "^":
+                existingLeft = grid[row+1][col-1]
+                existingRight = grid[row+1][col+1]
+                if existingLeft == "|":
+                    grid[row+1][col-1] = currentNum
+                else:
+                    grid[row+1][col-1] = grid[row+1][col-1] + currentNum
+
+                if existingRight == "|":
+                    grid[row+1][col+1] = currentNum
+                else:
+                    grid[row+1][col+1] = grid[row+1][col+1] + currentNum
+            else:
+                grid[row+1][col] = grid[row][col]
+            
+            printGrid(grid)
+            input("enter to con")
+
+# import copy
+# print("\n")
+# lastGrid = copy.deepcopy(grid)
+# count()
+# while grid != lastGrid:
+#     count()
+#     printGrid(grid)
+#     input("enter to con")
+    
+printGrid(grid)
+count()
+printGrid(grid)
+# ans = countPaths(grid, 0, grid[0].index("|"))
+# print("ans:", ans)
+
+lastRow = grid[-1]
+print("lastRow:", lastRow)
+lastNums = list(filter(lambda e: type(e) == int, lastRow))
+print("lastNums:", lastNums)
+print(sum(lastNums))
