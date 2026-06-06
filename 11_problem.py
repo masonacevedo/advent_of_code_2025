9e66002e4ba95d153e6ebd226c51acaf3e00d394
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-06-06 15:22:41 -0700

    11 part 1 done

---
+++ 11_problem.py
@@ -0,0 +1,42 @@
+import copy
+
+f_name = "11_input.txt"
+with open(f_name, "r+") as f:
+    lines = f.readlines()
+
+
+clean_lines = [line.replace("\n", "") for line in lines]
+
+adjList = {}
+for line in clean_lines:
+    node, *neighbors = line.split(" ")
+    node = node[0:-1]
+    adjList[node] = neighbors
+
+adjList['out'] = []
+
+for k,v in adjList.items():
+    print(k,"|",v)
+print()
+
+def findPaths(source, dest, adjList, currentPath = set()):
+    if dest in adjList[source]:
+        return [[source, dest]]
+    
+    if source in currentPath:
+        return None
+    
+    currentPath.add(source)
+    allPaths = []
+    for neighbor in adjList[source]:
+        pathsFromNeighbor = findPaths(neighbor, dest, adjList, currentPath)
+        if pathsFromNeighbor is None:
+            continue
+        newPaths = [[source] + copy.copy(p) for p in pathsFromNeighbor]
+        allPaths += newPaths
+    currentPath.remove(source)
+    return allPaths
+    
+ans = findPaths("you", "out", adjList)
+print("ans:", ans)
+print("len(ans):", len(ans))
