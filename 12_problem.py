7ec79dce7e6fe8582bf0a9f3f2575e648786122b
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-06-09 10:30:48 -0700

    12 part 1 memoized...

--- 12_problem.py
+++ 12_problem.py
@@ -101,7 +101,18 @@ AVAILABLE_SQUARES = set([(row, col) for row in range(0, NUMROWS) for col in rang
 def shapeSize(shape):
     return sum((row.count("#") for row in shape))
 
-def bestPlacements(shapeMap, grid, placementCounts, availableSquares):
+def bestPlacements(shapeMap, grid, placementCounts, availableSquares, memo):
+
+
+    memoKey = (frozenset(availableSquares), tuple(sorted(placementCounts.items())))
+    
+    
+    if memoKey in memo:
+        # print("using memo!")
+        # print(memoKey)
+        # input("enter to con")
+        return memo[memoKey]
+    # input("enter to con")
     
     placements = []
     counts = []
@@ -119,37 +130,41 @@ def bestPlacements(shapeMap, grid, placementCounts, availableSquares):
             newPlacementCounts = copy.copy(placementCounts)
             newPlacementCounts[shapeIndex] += 1
 
-            recPlacements, recCounts = bestPlacements(shapeMap, newGrid, newPlacementCounts, newAvailableSquares)
+            recPlacements, recCounts = bestPlacements(shapeMap, newGrid, newPlacementCounts, newAvailableSquares, memo)
             placements += recPlacements
             counts += recCounts
     
     if not(placementFound):
-        print("grid:")
-        for row in grid:
-            print(row)
-        print("placementCounts:", placementCounts)
-        input("base case. enter to con")
-        print()
-        return [grid], [placementCounts]
+        # print("grid:")
+        # for row in grid:
+        #     print(row)
+        # print("placementCounts:", placementCounts)
+        # input("base case. enter to con")
+        # print()
+        ans = [grid], [placementCounts]
+        memo[memoKey] = ans
+        return ans
 
 
-    print("grid:")
-    for row in grid:
-        print(row)
+    # print("grid:")
+    # for row in grid:
+    #     print(row)
 
-    print("returning:")
-    print("len(placements):", len(placements))
-    for p in placements:
-        for row in p:
-            print(row)
-        print()
-    print("len(counts):", len(counts))
-    for c in counts:
-        print(c)
+    # print("returning:")
+    # print("len(placements):", len(placements))
+    # for p in placements:
+    #     for row in p:
+    #         print(row)
+    #     print()
+    # print("len(counts):", len(counts))
+    # for c in counts:
+    #     print(c)
     
-    input("recursive case. enter to con")
-    print()
-    return placements, counts
+    # input("recursive case. enter to con")
+    # print()
+    ans = placements, counts
+    memo[memoKey] = ans
+    return ans
         
 
 
@@ -171,7 +186,7 @@ def bestPlacementsWrapper(shapeMap, numRows, numCols):
     availableSquares = set([(row, col) for row in range(0, numRows) for col in range(0, numCols)])
     # print("numRows:", numRows)
     # print("numCols:", numCols)
-    finalPlacements, finalCounts = bestPlacements(fullShapeMap, grid, placementCounts, availableSquares)
+    finalPlacements, finalCounts = bestPlacements(fullShapeMap, grid, placementCounts, availableSquares, memo = {})
     # print("ans:", ans)
     # input("ans should be complex, enter to con...")
     return finalPlacements, finalCounts
@@ -190,20 +205,17 @@ def findPlacements(shapes):
 def rotateShape(shape):
     return tuple(tuple(shape[col][row] for col in reversed(range(0, len(shape)))) for row in range(0, len(shape[0])))
 
-print("shapeMapping")
-print(shapeMapping[0])
-input("enter to start")
-ACTUAL_PLACEMENTS, COUNTS = bestPlacementsWrapper(shapeMapping, 4,4)
-# print("len(ACTUAL_PLACEMENTS):", len(ACTUAL_PLACEMENTS))
-# PAIRS = list(zip(ACTUAL_PLACEMENTS, COUNTS))
-# SORTED_PAIRS = sorted(PAIRS,
-#     key = lambda PAIR: sum(PAIR[1].values()),
-#     reverse=True
-# )
+ACTUAL_PLACEMENTS, COUNTS = bestPlacementsWrapper(shapeMapping, 6,4)
+print("len(ACTUAL_PLACEMENTS):", len(ACTUAL_PLACEMENTS))
+PAIRS = list(zip(ACTUAL_PLACEMENTS, COUNTS))
+SORTED_PAIRS = sorted(PAIRS,
+    key = lambda PAIR: sum(PAIR[1].values()),
+    reverse=True
+)
 
-# for p, c in SORTED_PAIRS:
-#     for row in p:
-#         print(row)
-#     for k,v in c.items():
-#         print(k,"|",v)
-#     input("enter to con")
+for p, c in SORTED_PAIRS:
+    for row in p:
+        print(row)
+    for k,v in c.items():
+        print(k,"|",v)
+    input("enter to con")
